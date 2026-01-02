9fb3041a67c9989bf07dfd724b360ae5d5663516
Gowreesh V T <135145326+Gowreesh-VT@users.noreply.github.com>
2026-01-02 11:54:04 +0530

    implement proxy.ts to work with vercel

--- src/proxy.ts
+++ src/proxy.ts
@@ -52,7 +52,7 @@ export async function proxy(request: NextRequest) {
 
     const cspDirectives = [
         `default-src 'self'`,
-        `script-src 'self' 'nonce-${nonce}' 'unsafe-eval' 'unsafe-inline'`,
+        `script-src 'self' 'unsafe-eval' 'unsafe-inline'`,
         `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
         `font-src 'self' https://fonts.gstatic.com`,
         `img-src 'self' data: https: blob:`,
