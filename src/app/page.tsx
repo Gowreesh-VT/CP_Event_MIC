8a9c24b7d7922e6a78ef11498071adbde01ee70f
Gowreesh V T <vt.gowreesh43@gmail.com>
2026-09-02 15:52:43 +0530

    steh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="min-h-screen bg-[#050505] text-white selection:bg-primary selection:text-black">
       <Navbar/>
       <HeroSection />
-      <Prizes />
+      {/* <Prizes /> */}
       {/* <Sponsors />
       <Organizers /> */}
       
